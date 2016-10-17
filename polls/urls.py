1cdd3665f350178d5158bc1867a73a99792d9d95
videet <videetssinghai@gmail.com>
2016-10-17 19:45:40 +0530

    1st push

---
+++ polls/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from . import views
+
+#url(r'^chat/$', views.chatApp, name='chatApp'),
+
+urlpatterns = [
+    url(r'^chatbotResponse/', views.chatbotResponse),
+    url(r'^$', views.chatApp),
+
+]
---
+++ polls/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+from django.template import loader
+# Create your views here.
+def chatApp(request):
+    template=loader.get_template("chat.html")
+    return HttpResponse(template.render())
+
+def chatbotResponse(request):
+    var = request.GET.get('sentence')
+    print str(var)
+    response_data ={
+        'key': str(var)
+    }
+    return JsonResponse(response_data)
+
+
